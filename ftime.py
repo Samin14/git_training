87602cc4b0d10ee754761cf61cbd9237c92e07d0
Manuel4131 <Manual131419@gmail.com>
2018-07-20 19:59:24 +0800

    1. Add weather procasting program 2. Add customized itme format

---
+++ ftime.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from datetime import date, time
+
+def today():
+	return date.today().strftime("%Y/%m/%d")
---
+++ weather.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import ftime
+
+def weather():
+	print("Current date and time are {}".format(ftime.today()))
+
+if __name__ == "__main__":
+	weather()
